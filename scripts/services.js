4217d079a8451be23b7249120dc54d2f2ef1d611
jklimpf <jklimpf@gmail.com>
2023-05-14 19:26:16 +0200

    added content to service page

--- scripts/services.js
+++ scripts/services.js
@@ -32,28 +32,53 @@ document.addEventListener("click", (event) => {
 
 // carousel
 
-let slidePosition = 1;
-SlideShow(slidePosition);
+let slidePositionPumpe = 1;
+let slidePositionCentirfugalnePumpe = 1;
+let slidePositionKatodnaZastita = 1;
+let slidePositionGuma = 1;
+let slidePositionTisak = 1;
+let slidePositionNamjestaj = 1;
+let slidePositionHidraulika = 1;
+
+SlideShowPumpe(slidePositionPumpe);
+SlideShowCentrifugalnePumpe(slidePositionCentirfugalnePumpe);
+SlideShowKatodnaZastita(slidePositionKatodnaZastita);
+SlideShowGuma(slidePositionGuma);
+SlideShowTisak(slidePositionTisak);
+SlideShowNamjestaj(slidePositionNamjestaj);
+SlideShowHidraulika(slidePositionHidraulika);
 
 // forward/Back controls
 function plusSlides(n) {
-  SlideShow((slidePosition += n));
+  SlideShowPumpe((slidePositionPumpe += n));
+  SlideShowCentrifugalnePumpe((slidePositionCentirfugalnePumpe += n));
+  SlideShowKatodnaZastita((slidePositionKatodnaZastita += n));
+  SlideShowGuma((slidePositionGuma += n));
+  SlideShowTisak((slidePositionTisak += n));
+  SlideShowNamjestaj((slidePositionNamjestaj += n));
+  SlideShowHidraulika((slidePositionHidraulika += n));
 }
 
 //  images controls
 function currentSlide(n) {
-  SlideShow((slidePosition = n));
+  SlideShowPumpe((slidePositionPumpe = n));
+  SlideShowCentrifugalnePumpe((slidePositionCentirfugalnePumpe = n));
+  SlideShowKatodnaZastita((slidePositionKatodnaZastita = n));
+  SlideShowGuma((slidePositionGuma = n));
+  SlideShowTisak((slidePositionTisak = n));
+  SlideShowNamjestaj((slidePositionNamjestaj = n));
+  SlideShowHidraulika((slidePositionHidraulika = n));
 }
 
-function SlideShow(n) {
+function SlideShowPumpe(n) {
   let i;
-  const slides = document.getElementsByClassName("Containers");
-  const circles = document.getElementsByClassName("dots");
+  const slides = document.getElementsByClassName("ContainersPumpe");
+  const circles = document.getElementsByClassName("dotsPumpe");
   if (n > slides.length) {
-    slidePosition = 1;
+    slidePositionPumpe = 1;
   }
   if (n < 1) {
-    slidePosition = slides.length;
+    slidePositionPumpe = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
@@ -61,6 +86,126 @@ function SlideShow(n) {
   for (i = 0; i < circles.length; i++) {
     circles[i].className = circles[i].className.replace(" enable", "");
   }
-  slides[slidePosition - 1].style.display = "block";
-  circles[slidePosition - 1].className += " enable";
+  slides[slidePositionPumpe - 1].style.display = "block";
+  circles[slidePositionPumpe - 1].className += " enable";
+}
+
+function SlideShowCentrifugalnePumpe(n) {
+  let i;
+  const slides = document.getElementsByClassName("ContainersCentrPumpe");
+  const circles = document.getElementsByClassName("dotsCentrPumpe");
+  if (n > slides.length) {
+    slidePositionCentirfugalnePumpe = 1;
+  }
+  if (n < 1) {
+    slidePositionCentirfugalnePumpe = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+    circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePositionCentirfugalnePumpe - 1].style.display = "block";
+  circles[slidePositionCentirfugalnePumpe - 1].className += " enable";
+}
+
+function SlideShowKatodnaZastita(n) {
+  let i;
+  const slides = document.getElementsByClassName("ContainersKatodnaZastita");
+  const circles = document.getElementsByClassName("dotsKatodnaZastita");
+  if (n > slides.length) {
+    slidePositionKatodnaZastita = 1;
+  }
+  if (n < 1) {
+    slidePositionKatodnaZastita = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+    circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePositionKatodnaZastita - 1].style.display = "block";
+  circles[slidePositionKatodnaZastita - 1].className += " enable";
+}
+
+function SlideShowGuma(n) {
+  let i;
+  const slides = document.getElementsByClassName("ContainersGuma");
+  const circles = document.getElementsByClassName("dotsGuma");
+  if (n > slides.length) {
+    slidePositionGuma = 1;
+  }
+  if (n < 1) {
+    slidePositionGuma = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+    circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePositionGuma - 1].style.display = "block";
+  circles[slidePositionGuma - 1].className += " enable";
+}
+
+function SlideShowTisak(n) {
+  let i;
+  const slides = document.getElementsByClassName("ContainersTisak");
+  const circles = document.getElementsByClassName("dotsTisak");
+  if (n > slides.length) {
+    slidePositionTisak = 1;
+  }
+  if (n < 1) {
+    slidePositionTisak = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+    circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePositionTisak - 1].style.display = "block";
+  circles[slidePositionTisak - 1].className += " enable";
+}
+
+function SlideShowNamjestaj(n) {
+  let i;
+  const slides = document.getElementsByClassName("ContainersNamjestaj");
+  const circles = document.getElementsByClassName("dotsNamjestaj");
+  if (n > slides.length) {
+    slidePositionNamjestaj = 1;
+  }
+  if (n < 1) {
+    slidePositionNamjestaj = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+    circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePositionNamjestaj - 1].style.display = "block";
+  circles[slidePositionNamjestaj - 1].className += " enable";
+}
+
+function SlideShowHidraulika(n) {
+  let i;
+  const slides = document.getElementsByClassName("ContainersHidraulika");
+  const circles = document.getElementsByClassName("dotsHidraulika");
+  if (n > slides.length) {
+    slidePositionHidraulika = 1;
+  }
+  if (n < 1) {
+    slidePositionHidraulika = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+    circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePositionHidraulika - 1].style.display = "block";
+  circles[slidePositionHidraulika - 1].className += " enable";
 }
